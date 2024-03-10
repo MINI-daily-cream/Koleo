5f32ae484e787de412bd9f83914928baaffe83da
glowacz <glowacki.pj@gmail.com>
2024-03-10 12:08:07 +0100

    classes scaffold

---
+++ KoleoPL/Services/ComplaintService.cs
@@ -0,0 +1,22 @@
+﻿namespace KoleoPL.Services
+{
+    public class ComplaintService
+    {
+        public void MakeComplaint() // logika dla odpowiedniej metody w User
+        {
+
+        }
+
+        public void EditComplaint() // logika dla odpowiedniej metody w User
+        {
+
+        }
+
+        public void RemoveComplaint() // logika dla odpowiedniej metody w User
+        {
+
+        }
+
+        // dla Admina też powinniśmy robić logikę w Services?
+    }
+}
---
+++ KoleoPL/Services/TicketService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace KoleoPL.Services
+{
+    public class TicketService
+    {
+        public void Buy() // pozwala na zakup biletu
+        {
+            PaymentService paymentService = new PaymentService(); // potem będzie jako dependency
+            // (dependency injection w konstruktorze)
+            paymentService.ProceedPayment();
+        }
+
+        public void ListByUser() // umożliwia ka˙zdemu u˙zytkownikowi wgl ˛ad w jego bilety
+        {
+        }
+
+        public void Generate() // tworzy plik pdf z biletem
+        {
+            
+        }
+
+        public void Remove() // usuwa bilet z bazy danych i ponownie ł ˛aczy si˛e z PaymentService w celu zwrócenia pieni˛edzy
+        // potrzeba dodatkowej metody w PaymentService ?
+        {
+            PaymentService paymentService = new PaymentService(); // potem będzie jako dependency
+            // (dependency injection w konstruktorze)
+            paymentService.ProceedPayment();
+        }
+
+        public void Add() // jest wywoływana przez Buy() i dodaje bilet do bazy danych
+        {
+
+        }
+
+        public void ChangeDetails() // pozwala dokonac zmiany danych na bilecie i ponownie korzysta z metody Generate()
+        {
+
+        }
+    }
+}

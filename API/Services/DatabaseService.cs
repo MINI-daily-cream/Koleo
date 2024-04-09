25f366e2e7def3c5a16f7e5c9e3ae7f6bee24960
glowacz <glowacki.pj@gmail.com>
2024-04-10 01:13:30 +0200

    DatabaseService - working ExecuteSQL (backup not working)

--- API/Services/DatabaseService.cs
+++ API/Services/DatabaseService.cs
@@ -1,17 +1,66 @@
-namespace Koleo.Services
+using Microsoft.Data.SqlClient;
+using Microsoft.Data.Sqlite;
+using System.Reflection.PortableExecutable;
+
+namespace KoleoPL.Services
 {
     public class DatabaseService
     {
-        public DatabaseService()
+        public IConfiguration Configuration { get; }
+        public DatabaseService(IConfiguration configuration)
         {
+            Configuration = configuration;
         }
-        public void ExecuteSQL()
+        public async Task<List<string[]>> ExecuteSQL(string sql)
         {
+            var results = new List<string[]>();
 
-        }
-        public void Backup()
-        {
+            await using var conn = new SqliteConnection(Configuration.GetConnectionString("DefaultConnection"));
+
+            await conn.OpenAsync();
+
+            await using var cmd = new SqliteCommand(sql, conn);
+
+            await using var dataReader = await cmd.ExecuteReaderAsync();
+
+            while(await dataReader.ReadAsync())
+            {
+                //Console.WriteLine(dataReader["Name"]);
+                //Console.WriteLine(dataReader["Surname"]);
+
+                var values = new string[dataReader.FieldCount];
+                dataReader.GetValues(values);
+                results.Add(values);
+            }
             
+              
+            return results;
+        }
+        public async void Backup()
+        {
+            string backupPath = "C:\\KoleoBackup.bak",
+                sql = $"VACUUM INTO '{backupPath}'";
+                //sql = $"BACKUP DATABASE YourDatabase TO DISK = '{backupPath}'";
+
+            //using (var location = new SqliteConnection(Configuration.GetConnectionString("DefaultConnection")))
+            //using (var destination = new SqliteConnection(string.Format($"Data Source={backupPath};")))
+            //{
+            //    location.Open();
+            //    destination.Open();
+            //    location.BackupDatabase(destination, "main", "main", -1, null, 0);
+            //}
+
+            
+
+            //string connectionString = "Data Source=YourServer;Initial Catalog=YourDatabase;Integrated Security=True";
+
+            await using var conn = new SqliteConnection(Configuration.GetConnectionString("DefaultConnection"));
+
+            await conn.OpenAsync();
+
+            await using var cmd = new SqliteCommand(sql, conn);
+
+            await cmd.ExecuteNonQueryAsync();
         }
     }
 }

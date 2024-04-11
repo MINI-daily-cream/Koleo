f5879bba78ccbae37e66fba9331531cf3471eb5c
glowacz <glowacki.pj@gmail.com>
2024-04-11 22:25:36 +0200

    backup working (non-queries also worked already)

--- API/Services/DatabaseService.cs
+++ API/Services/DatabaseService.cs
@@ -38,29 +38,14 @@ namespace KoleoPL.Services
         }
         public async void Backup()
         {
-            string backupPath = "C:\\KoleoBackup.bak",
-                sql = $"VACUUM INTO '{backupPath}'";
-                //sql = $"BACKUP DATABASE YourDatabase TO DISK = '{backupPath}'";
+            string backupDb = "KoleoBackup.db";
 
-            //using (var location = new SqliteConnection(Configuration.GetConnectionString("DefaultConnection")))
-            //using (var destination = new SqliteConnection(string.Format($"Data Source={backupPath};")))
-            //{
-            //    location.Open();
-            //    destination.Open();
-            //    location.BackupDatabase(destination, "main", "main", -1, null, 0);
-            //}
-
-            
-
-            //string connectionString = "Data Source=YourServer;Initial Catalog=YourDatabase;Integrated Security=True";
+            var backup = new SqliteConnection($"Data Source={backupDb}");
 
             await using var conn = new SqliteConnection(Configuration.GetConnectionString("DefaultConnection"));
-
             await conn.OpenAsync();
 
-            await using var cmd = new SqliteCommand(sql, conn);
-
-            await cmd.ExecuteNonQueryAsync();
+            conn.BackupDatabase(backup);
         }
     }
 }

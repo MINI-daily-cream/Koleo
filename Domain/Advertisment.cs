c6022b6b704d2e5d305a0d03ac1cf24c1d4fca0a
merbich <grishko.alina1@mail.ru>
2024-04-14 21:14:32 +0200

    advertisement table migration

--- Domain/Advertisment.cs
+++ Domain/Advertisment.cs
@@ -4,9 +4,10 @@
     {
         public Guid Id { get; set; }
         public string AdContent { get; set; }
-        public string AdLink { get; set; }
-        public byte[] AdImage { get; set; }
+        public string AdLinkUrl { get; set; }
+        public string AdImageUrl { get; set; }
         public AdvertismentCategory AdCategory { get; set; }
+        public string AdOwner { get; set; }
     }
 
     public enum AdvertismentCategory
--- Domain/MainComponents/Server.cs
+++ Domain/MainComponents/Server.cs
@@ -1,6 +1,22 @@
-﻿namespace Koleo.Models
+﻿using System.Text;
+
+namespace Koleo.Models
 {
     public class Server
     {
+        List<Advertisment> GetAds(List<AdvertismentCategory> categories)
+        {
+            StringBuilder sql = new StringBuilder();
+            sql.Append("SELECT * FROM Advertisment WHERE ");
+            foreach (var category in categories)
+            {
+                sql.Append($"AdvertismentCategory='{category.ToString()}' and ");
+            }
+            int startIndex = sql.Length - 5;
+            sql.Remove(sql.Length - 5, 5);
+
+
+            return new List<Advertisment>();
+        }
     }
 }

effe6bc1f5b39f0f38d5006910f064ed89a29392
glowacz <glowacki.pj@gmail.com>
2024-04-27 16:37:02 +0200

    working AccountController, some of TicketController

--- API/Services/Interfaces/IAccountService.cs
+++ API/Services/Interfaces/IAccountService.cs
@@ -1,6 +1,10 @@
-﻿namespace API.Services.Interfaces
+﻿using Domain;
+
+namespace API.Services.Interfaces
 {
     public interface IAccountService
     {
+        public Task<AccountInfo>? GetAccountInfo(string userId);
+        public Task<bool> UpdateAccountInfo(string userId, string newName, string newSurname, string newEmail);
     }
 }
--- API/Services/Interfaces/ITicketServive.cs
+++ API/Services/Interfaces/ITicketServive.cs
@@ -5,8 +5,8 @@ namespace API.Services.Interfaces
 {
     public interface ITicketServive
     {
-        public Task<bool> Buy(Guid userId, List<Connection> connections, string targetName, string targetSurname);
+        public Task<bool> Buy(string userId, List<Connection> connections, string targetName, string targetSurname);
         public Task<List<ConnectionInfoObject>> ListByUser(int userId);
-        public Task Add(Guid userId, List<Connection> connections, string targetName, string targetSurname);
+        public Task Add(string userId, List<Connection> connections, string targetName, string targetSurname);
     }
 }

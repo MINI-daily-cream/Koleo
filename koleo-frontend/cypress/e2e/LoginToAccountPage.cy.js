7796ae16994e391012ef401a105cb3780d26ae9b
Piotr Baranowski <piotr.baranowski250@gmail.com>
2024-05-07 09:27:53 +0200

    -

--- koleo-frontend/cypress/e2e/LoginToAccountPage.cy.js
+++
@@ -1,9 +0,0 @@
-describe("LoginPage", () => {
-  it("should redirect to account page when login button is clicked", () => {
-    cy.visit(Cypress.config().baseUrl + "/login");
-
-    cy.get("button").contains("Zaloguj się").click();
-
-    cy.url().should("eq", Cypress.config().baseUrl + "/account");
-  });
-});
--- koleo-frontend/cypress/e2e/LogoutButton.cy.js
+++
@@ -1,9 +0,0 @@
-describe("Logout", () => {
-  it("should redirect to home page when logout button is clicked", () => {
-    cy.visit(Cypress.config().baseUrl + "/account");
-
-    cy.contains("Wyloguj się").click();
-
-    cy.url().should("eq", Cypress.config().baseUrl + "/");
-  });
-});

df1915809600f3c11dad5d0eb96edd699018fdfd
wuujt <wojtas.domis@gmail.com>
2024-05-08 06:35:44 +0200

    some controllers domains and migration

---
+++ server/API/Controllers/RankingController.cs
@@ -0,0 +1,25 @@
+﻿using API.Services.Interfaces;
+using Domain;
+using Koleo.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class RankingController : ControllerBase
+    {
+        private readonly IRankingService _RankingService;
+
+        public RankingController(IRankingService rankingService)
+        {
+            _RankingService = rankingService;
+        }
+
+        [HttpGet("{id}")]
+        public Task<List<RankingInfo>> Get(string id)
+        {
+            return _RankingService.GetByUser(id);
+            
+        }
+
+    }
+}
---
+++ server/API/Services/Interfaces/IStatisticsService.cs
@@ -0,0 +1,12 @@
+﻿using Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Services.Interfaces
+{
+    public interface IStatisticsService
+    { 
+            Task<StatisticsInfo>? GetByUser(string userID);
+             void Update(string userID, ConnectionInfoObject connectionInfoObject);
+        
+    }
+}
